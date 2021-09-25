437f7338e56e44b9ac0dd499645514737b875fdf
Beno Zupanc <zupanc.beno@gmail.com>
2021-09-25 22:23:57 +0200

    heroku db updates

--- __init__.py
+++ __init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
 
 db = SQLAlchemy()
-
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -27,5 +30,23 @@ def create_app(config_class=Config):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/ginger-alerts.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('ginger-alerts startup')
 
     return app
--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Potejto#"1323))(Poutatoučćč'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'not-so-secret-key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///ginger-alerts.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or 'sqlite:///' + os.path.join(basedir, 'ginger-alerts.db')
 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
